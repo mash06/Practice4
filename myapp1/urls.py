2a94957436789f9c8fd28ea498f90b08393e4f1f
mash06 <mashulia200106@mail.ru>
2020-07-20 18:14:49 +0400

    github

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index,name='index'),
-    path('SECOND', views.podschet,name='podschet'),
+    path('podschet', views.podschet,name='podschet'),
 ]
